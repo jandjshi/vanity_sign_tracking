81e9bcdaa4c002854bc4ea8d83b3da6367754874
jeff <jeff@2redbeans.com>
2011-04-23 00:09:30 -0700

    First commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source :rubygems
+gemspec
+
+group :development do
+  gem "jekyll"
+  gem "rake"
+  gem "RedCloth"
+  gem "yard"
+end
+
+group :test do
+  gem "bson_ext"
+  gem "garb"
+  gem "mocha"
+  gem "mongo"
+  gem "passenger"
+  gem "rails", "2.3.8"
+  gem "rack", "1.1.0"
+  gem "shoulda"
+  gem "sqlite3-ruby", "1.2.5" # 1.3.0 doesn't like Ruby 1.9.1
+  gem "timecop"
+  #gem "SystemTimer"
+  gem "webmock"
+end
---
+++ lib/vanity.rb
@@ -0,0 +1,36 @@
+require "date"
+require "time"
+require "logger"
+require "cgi"
+require "erb"
+require "yaml"
+
+# All the cool stuff happens in other places.
+# @see Vanity::Helper
+# @see Vanity::Rails
+# @see Vanity::Playground
+# @see Vanity::Metric
+# @see Vanity::Experiment
+module Vanity
+end
+
+require "vanity/version"
+require "vanity/backport" if RUBY_VERSION < "1.9"
+# Metrics.
+require "vanity/metric/base"
+require "vanity/metric/active_record"
+require "vanity/metric/google_analytics"
+require "vanity/metric/remote"
+# Experiments.
+require "vanity/experiment/base"
+require "vanity/experiment/ab_test"
+# Database adapters
+require "vanity/adapters/abstract_adapter"
+require "vanity/adapters/redis_adapter"
+require "vanity/adapters/mongodb_adapter"
+require "vanity/adapters/mock_adapter"
+# Playground.
+require "vanity/playground"
+require "vanity/helpers"
+# Integration with various frameworks.
+require "vanity/frameworks/rails" if defined?(Rails)
